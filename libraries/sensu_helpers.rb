6fc12e1c5105b43abad78f915569081bb55d960a
jbrook <james.brook@spaaza.com>
2014-08-19 20:25:08 +0200

    Make data_bag_item helper work with Opsworks.

--- libraries/sensu_helpers.rb
+++ libraries/sensu_helpers.rb
@@ -33,7 +33,7 @@ module Sensu
       end
 
       def data_bag_item(item, missing_ok=false)
-        raw_hash = Chef::DataBagItem.load("sensu", item)
+        raw_hash = data_bag_item("sensu", item)
         encrypted = raw_hash.detect do |key, value|
           if value.is_a?(Hash)
             value.has_key?("encrypted_data")
